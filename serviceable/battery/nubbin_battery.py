945d12e84f4051b631f0afb023edf8becfab07a2
Oluwafemisire <oluwafemisire.ojuawo@kibo.school>
2023-07-11 20:23:58 +0100

    time management

--- serviceable/battery/nubbin_battery.py
+++ serviceable/battery/nubbin_battery.py
@@ -1,4 +1,5 @@
-from battery.battery import Battery
+from serviceable.battery.battery import Battery
+from utils import add_years_to_date
 
 class NubbinBattery(Battery):
     def __init__(self, last_service_date,current_service_date):
@@ -6,4 +7,8 @@ class NubbinBattery(Battery):
         self._current_service_date = current_service_date
 
     def needs_service(self):
-        return self._current_service_date - self._last_service_date > 4
+        proposed_service_date = add_years_to_date(self._last_service_date, 4) 
+        if proposed_service_date < self._current_service_date:
+            return True
+        else:
+            return False
